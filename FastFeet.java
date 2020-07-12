d3d508616a906d5c950e9b09b5335c5d8f0f72b6
raj <arushrayjada@gmail.com>
2020-07-13 01:20:24 +0530

    Added new Functionality:Used Inheritence to creates separeate classes then extending the base class bicycle to reuse the code

--- FastFeet.java
+++ FastFeet.java
@@ -1,8 +1,51 @@
 package learnjava;
 
-class bicycle
+class Bicycle
 {
+	private double size;
+	private String chain;
+	private double tireSize;
 
+	Bicycle(double size,String chain,double tireSize )
+	{
+		this.size=size;
+		this.chain=chain;
+		this.tireSize=tireSize;
+	}
+}
+
+class RoadBike extends Bicycle
+{
+	String tapeColor;
+
+	RoadBike(String tapeColor,double size,String chain,double tireSize)
+	{
+		super(size,chain,tireSize);
+		this.tapeColor=tapeColor;
+	}
+
+	String getTapeColor()
+	{
+		return tapeColor;
+	}
+}
+
+class MountainBike extends Bicycle
+{
+	double frontShock;
+	double rearShock;
+
+	MountainBike(double frontShock, double rearShock,double size,String chain,double tireSize)
+	{
+		super(size,chain,tireSize);
+		this.frontShock=frontShock;
+		this.rearShock=rearShock;
+	}
+
+	double mergeShock()
+	{
+		return frontShock+rearShock;
+	}
 }
 
 public class FastFeet
