d29471bbe1eafbb09d439023c4d6a03a89a08ec0
raj <arushrayjada@gmail.com>
2020-07-12 22:47:20 +0530

    Added feature: Introduce the capability to take into account the tire size for calculating distance tavelled

--- Basic.java
+++ Basic.java
@@ -5,24 +5,33 @@ class Gear
 {
 	private int chainring;
 	private int cog;
+	private double rimDiameter;
+	private double tireDiameter;
 
-	Gear(int chainring, int cog)
+	Gear(int chainring, int cog,double rimDiameter, double tireDiameter)
 	{
 		this.chainring=chainring;
 		this.cog=cog;
+		this.rimDiameter =rimDiameter;
+		this.tireDiameter=tireDiameter;
 	}
 
 	double getRatio()
 	{
 		return chainring/(double)cog;
 	}
+
+	double getGearInches()
+	{
+		return(getRatio()*(rimDiameter+(tireDiameter*2)));
+	}
 }
 
 class Basic
 {
 	public static void main(String[] args) {
-		Gear g1 = new Gear(52,11);
-		double ratio = g1.getRatio();
-		System.out.println(ratio);
+		Gear g1 = new Gear(52,11,26,1.5);
+		double GI = g1.getGearInches();
+		System.out.println(GI);
 	}
 }
